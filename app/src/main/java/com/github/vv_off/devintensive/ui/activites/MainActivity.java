8c0a55f8c56c4a44adc7f9cd8813a7bdab18aee1
vv-off <vv-off83@mail.ru>
2016-09-19 21:12:00 +0300

    Initial commit

---
+++ app/src/main/java/com/github/vv_off/devintensive/ui/activites/MainActivity.java
@@ -0,0 +1,15 @@
+package com.github.vv_off.devintensive.ui.activites;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.vv_off.devintensive.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
