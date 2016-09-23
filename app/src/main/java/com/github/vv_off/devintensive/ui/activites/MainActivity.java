c60a8e312ba8ad9a7b21e1e6f1b8c9ae2ebc2c14
vv-off <vv-off83@mail.ru>
2016-09-23 22:44:46 +0300

    вернул названия colors

--- app/src/main/java/com/github/vv_off/devintensive/ui/activites/MainActivity.java
+++ app/src/main/java/com/github/vv_off/devintensive/ui/activites/MainActivity.java
@@ -1,15 +1,106 @@
 package com.github.vv_off.devintensive.ui.activites;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.github.vv_off.devintensive.R;
+import com.github.vv_off.devintensive.utils.ConstantManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private static final String TAG = ConstantManager.TAG_PREFIX + "Main Activity";
+
+    protected EditText mEditText;
+    protected Button mRedButton, mGreenButton;
+    protected int mColorMode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate");
+
+        mRedButton = (Button) findViewById(R.id.red_btn);
+        mGreenButton = (Button) findViewById(R.id.green_btn);
+        mEditText = (EditText) findViewById(R.id.edit_text);
+
+        mRedButton.setOnClickListener(this);
+        mGreenButton.setOnClickListener(this);
+
+        if(savedInstanceState == null){
+            //активити запускается впервые
+        } else {
+            //активити уже создавалось
+            mColorMode = savedInstanceState.getInt(ConstantManager.COLOR_MODE_KEY);
+
+            if(mColorMode == Color.RED){
+                mEditText.setBackgroundColor(Color.RED);
+            } else if (mColorMode == Color.GREEN){
+                mEditText.setBackgroundColor(Color.GREEN);
+            }
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.red_btn:
+                mEditText.setBackgroundColor(Color.RED);
+                mColorMode = Color.RED;
+                break;
+            case R.id.green_btn:
+                mEditText.setBackgroundColor(Color.GREEN);
+                mColorMode = Color.GREEN;
+                break;
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Log.d(TAG,"onSaveInstanceState");
+        outState.putInt(ConstantManager.COLOR_MODE_KEY, mColorMode);
     }
 }
---
+++ app/src/main/java/com/github/vv_off/devintensive/utils/ConstantManager.java
@@ -0,0 +1,6 @@
+package com.github.vv_off.devintensive.utils;
+
+public interface ConstantManager {
+    String TAG_PREFIX = "DEV";
+    String COLOR_MODE_KEY = "COLOR_MODE_KEY";
+}
